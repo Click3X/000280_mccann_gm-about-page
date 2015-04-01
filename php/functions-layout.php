7823a8a046706160f3863f4070b278b22e584b14
Charles Davis <charlesdavis@cfmmacbook1.local>
2015-04-01 15:08:25 -0400

    added alt tags to imgs

--- php/functions-layout.php
+++ php/functions-layout.php
@@ -9,7 +9,7 @@ function pageHeader($title, $icon, $tagline, $image) {
 	echo '<div class="c3xgm-about-section c3xgm-about-clearfix '.cleanString($title).'">
 		    <div class="c3xgm-about-section-header c3xgm-about-clearfix">
 			    <div class="c3xgm-about-section-header-inner">
-			        <img class="c3xgm-about-section-icon" src="'.$icon.'">
+			        <img class="c3xgm-about-section-icon" src="'.$icon.'" alt="'.$title.'">
 			        <h1 class="c3xgm-about-h">'.$titleSpan.'</h1>
 			    </div>
 				<hr>
@@ -17,7 +17,7 @@ function pageHeader($title, $icon, $tagline, $image) {
 
 			if($image) { 
 				$size = getimagesize($image);
-				echo '<div class="c3xgm-about-header-image" style="max-width:'.$size[0].'px; max-height:'.$size[1].'px;"><img src="'.$image.'"></div>';
+				echo '<div class="c3xgm-about-header-image" style="max-width:'.$size[0].'px; max-height:'.$size[1].'px;"><img src="'.$image.'" alt="'.$title.'"></div>';
 			}
 
 			if($tagline) { 
@@ -93,7 +93,7 @@ function printBlock($block) {
 		if( isset($block['image@2x']) ) { 
 			if( isset($block['image']) && ($block['image'] != "") ) { 
 				$size = getimagesize($block['image']); 
-				echo '<div class="c3xgm-about-block-image"><img src="'.$block['image@2x'].'"></div>';
+				echo '<div class="c3xgm-about-block-image"><img src="'.$block['image@2x'].'" alt="'.$block['title'].'"></div>';
 			}
 		}
 	echo '</div>';
@@ -127,7 +127,7 @@ function printModule($module) {
 					foreach ($module['blocks'] as $key => $block) {
 						if( isset($block['logo']) ) {
 							$link = cleanString($block['title']);
-							echo '<li><a href="#c3xgm-about-'.$link.'" class="c3xgm-about-module-car-logo"><img src="'.$block['logo'].'"></a></li>';
+							echo '<li><a href="#c3xgm-about-'.$link.'" class="c3xgm-about-module-car-logo"><img src="'.$block['logo'].'" alt="'.$block['title'].'"></a></li>';
 						}
 					}
 				echo '</ul>';
@@ -144,7 +144,7 @@ function printModule($module) {
 					foreach ($module['blocks'] as $key => $block) {
 						if( isset($block['logo']) ) {
 							$link = cleanString($block['title']);
-							echo '<li><a href="#c3xgm-about-'.$link.'" class="c3xgm-about-module-foundation-logo"><img src="'.$block['logo'].'"></a></li>';
+							echo '<li><a href="#c3xgm-about-'.$link.'" class="c3xgm-about-module-foundation-logo"><img src="'.$block['logo'].'" alt="'.$block['title'].'"></a></li>';
 						}
 					}
 				echo '</ul>';
@@ -177,6 +177,7 @@ function printModule($module) {
 						}
 						echo '</p>';
 					} else {
+						// ECHO COPY
 						echo '<p class="c3xgm-about-p">'.$block['copy'].'</p>';	
 					}
 	 			}
@@ -187,7 +188,7 @@ function printModule($module) {
 	 			// ICON
 	 			if( (isset($block['icon']) )  && ($block['icon'] != "") ) {
 	 				echo '<div class="c3xgm-about-clearfix c3xgm-about-block-icon">';
-	 					echo '<img src="'.$block['icon'].'">';
+	 					echo '<img src="'.$block['icon'].'" alt="'.$block['title'].'">';
 	 				echo '</div>';
 	 			}
 
@@ -195,12 +196,12 @@ function printModule($module) {
 	 			if($module_name == 'car') {
 		 			if( (isset($block['image']) )  && ($block['image'] != "") ) {
 		 				echo '<div class="c3xgm-about-clearfix c3xgm-about-block-image">';
-		 					echo '<img src="'.$block['image'].'">';
+		 					echo '<img src="'.$block['image'].'" alt="'.$block['title'].'">';
 		 				echo '</div>';
 		 			}
 		 		} elseif ($module_name == 'technology') {
 		 			echo '<div class="c3xgm-about-clearfix c3xgm-about-block-image">';
-		 				echo '<img src="'.$block['image'].'">';
+		 				echo '<img src="'.$block['image'].'" alt="'.$block['title'].'">';
 		 			echo '</div>';
 		 		}	 			
 	 		echo '</div>'; // END MODULE BLOCK
