f26bbe81b3aba0756f0278f07f7036232fe1ffd4
Charles Davis <charlesdavis@cfmmacbook1.local>
2015-05-28 17:46:39 -0400

    Fixed CTD svg, gif differences and also added first auto rotate on tech slider

--- js/helpers.js
+++ js/helpers.js
@@ -23,9 +23,16 @@ if(mobile) {
     // REPLACE GIFS WITH SVGS
     $('img[src$=".gif"]').each(function(index,element) {
         // NO CHINA FLAG GIF FOR MOBILE, BUT IF SCREEN IS IPAD OR LARGER, SHOW GIF
-        if( (element.src != 'chinaflag.gif') && ( $(window).width < 768 )) {
+        console.log('This is element Src: ' + element.src);
+        console.log('This is window Width: ' + $(window).width() );
+        
+        if( (element.src != 'chinaflag.gif') && ($(window).width() < 768) ) {
+            
             element.src = element.src.replace('.gif','.svg');
-        }
+        } 
+        // else if( element.src == 'crash-test-dummies.gif' ) {
+        //     // element.src = element.src.replace('.gif','.svg');
+        // }
     });
 
      // MAKE FLAGS ANIMATE
