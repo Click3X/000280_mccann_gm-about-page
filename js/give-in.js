0067ba78443ef139b34669c686721c96d72f107b
Charlie Davis <charliekuldip@gmail.com>
2015-06-25 13:13:45 -0400

    Just added code to autoRotate Sliders and kill them when they come in and out of view

--- js/give-in.js
+++ js/give-in.js
@@ -15,12 +15,15 @@ jQuery(document).ready(function($) {
     // BLOCKS FOR BLOCK IN VIEW ANIMATIONS
     var pages = $('.c3xgm-about-page'),
     	blocks = $('.c3xgm-about-block').not('.c3xgm-about-solar-panels-animation, .c3xgm-about-test-facility'),
+    	sliders = $('.c3xgm-about-module-foundation, .c3xgm-about-module-technology, .c3xgm-about-module-car'),
     	triggers = $('#animate-flag-line, #c3xgm-about-solar-road-list');
 
-    console.log('These are your blocks:');
-    console.dir(blocks);
     console.log('These are your pages:');
     console.dir(pages);
+    console.log('These are your blocks:');
+    console.dir(blocks);
+    console.log('These are your sliders:');
+    console.dir(sliders);
     console.log('These are your triggers:');
     console.dir(triggers);
 
@@ -38,6 +41,33 @@ jQuery(document).ready(function($) {
 	});
 
 
+    // SLIDERS
+	$(sliders).bind('inview', function (event, visible) {
+    	var name = this.className.split(" ")[1];
+		if (visible == true) {
+			console.log('Im in view!: ' + name);
+			if(name == 'c3xgm-about-module-car') {
+				autorotateAboutSlider();
+			} else if(name == 'c3xgm-about-module-technology') {
+				autorotateTechSlider();
+			} else if (name == 'c3xgm-about-module-foundation') {
+				autorotateFoundationSlider();
+			}
+		} else {
+			console.log('Im out of view!: ' + name);
+			if(name == 'c3xgm-about-module-car') {
+				killAutoRotateAboutSlider();
+			} else if(name == 'c3xgm-about-module-technology') {
+				killAutoRotateTechSlider();
+			} else if (name == 'c3xgm-about-module-foundation') {
+				killAutoRotateFoundationSlider();
+			}
+		}
+	});
+
+
+
+    // TRIGGER FUNCTION ON WINDOW AT DOCUMENT READY
 	$(window).trigger('scroll');
 
 });
