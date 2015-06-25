c7871e17d16960a55bab9ab0ea6bd2ff9b68cb9e
Charlie Davis <charliekuldip@gmail.com>
2015-06-25 17:27:37 -0400

    Added our people animations

--- js/give-in.js
+++ js/give-in.js
@@ -1,9 +1,149 @@
 // GIVE IN  
+// OUR PEOPLE VARS
 // DECLARE VARS
 var currentPage = 0,
 	currentSection = 0;
 
+// NUMBER ANIMATIONS
+var current_frame, total_frames, path, length, handle;
+
+var init = function() {
+  current_frame = 0;
+  total_frames = 480;
+  path = new Array();
+  length = new Array();
+  // ITERATE THROUGH PATHS
+  for(var i=0; i< 12 ;i++){
+    // GET PATH
+    path[i] = document.getElementById('i'+i);
+
+    // GET PATH LENGTH
+    l = path[i].getTotalLength();
+    // ADD TO LENGTH ARRAY
+    length[i] = l;
+    // CSS PROPERTIES THAT WE ARE ANIMATING
+    path[i].style.strokeDasharray = l + ' ' + l; 
+    path[i].style.strokeDashoffset = l;
+  }
+  handle = 0;
+}
+ 
+ 
+var draw = function() {
+   var progress = current_frame/total_frames;
+   if (progress > 1) {
+        // window.cancelAnimationFrame(handle);
+        clearTimeout(handle);
+   } else {
+     current_frame++;
+     for(var j=0; j<path.length;j++){
+         path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
+     }
+     // handle = window.requestAnimationFrame(draw);
+     handle = setTimeout(function(){
+        draw();
+     }, 60/1000);
+   }
+};
+
 jQuery(document).ready(function($) { 
+    
+    var animateEmployees = false;
+    function fireAnimatedEmployees() {
+          if(animateEmployees === false) {
+            // EMPTY TEXT
+            $('#c3xgm-about-emp-num').text('');
+            // TRIGGER NUMBER ANIMATION
+            $('#c3xgm-about-emp-num').jQuerySimpleCounter({end: 216000,duration: 2500});
+            // MAKE SURE NUMBER REACHES 216000
+            setTimeout(function(){ 
+                $('#c3xgm-about-emp-num').text(formatNumber(216000));
+            }, 2500);
+            // TRIGGER PEOPLE ANIMATION
+            $('#c3xgm-about-employees-img div').each(addAnimation);
+            animateEmployees = true;
+        }
+    }
+
+
+    var animate6 = false;
+    function fireAnimate6() {          
+          if(animate6 === false) {
+            // EMPTY TEXT
+            $('#c3xgm-about-number-6').text('');
+          // // TRIGGER NUMBER ANIMATION
+            setTimeout(function(){ 
+                init();
+                draw();
+                $('#c3xgm-about-number-6').jQuerySimpleCounter({start:0, end: 6,duration: 1000});
+                animate6 = true;
+            }, 800);
+        }
+    }
+
+    var animate23 = false;
+    function fireAnimate23() {
+          if(animate23 === false) {
+            // EMPTY TEXT
+            $('#c3xgm-about-number-23').text('');
+            // // TRIGGER NUMBER ANIMATION
+            setTimeout(function(){ 
+                $('#c3xgm-about-number-23').jQuerySimpleCounter({start:0, end: 23,duration: 1200});
+                animate23 = true;
+            }, 600);
+        }
+    }
+
+    var animate70 = false;
+    function fireAnimate70() {
+          if(animate70 === false) {
+            // EMPTY TEXT
+            $('#c3xgm-about-number-70').text('');
+          // // TRIGGER NUMBER ANIMATION
+            setTimeout(function(){ 
+                $('#c3xgm-about-number-70').jQuerySimpleCounter({start:0, end: 70,duration: 1200});
+                animate70 = true;
+            }, 600);
+        }
+    }
+
+    var animate63 = false;
+    function fireAnimate63() {
+          if(animate63 === false) {
+            // EMPTY TEXT
+            $('#c3xgm-about-number-63').text('');
+          // // TRIGGER NUMBER ANIMATION
+            setTimeout(function(){ 
+                $('#c3xgm-about-number-63').jQuerySimpleCounter({start:40, end: 63,duration: 900});
+                animate63 = true;
+            }, 800);
+        }
+    }
+
+    fireOurPeopleAnimations = function(page) {
+    	var time;
+
+        $.each(ourPeopleBlocks, function(i, val) {
+            setTimeout(function() {
+                time = 1200 * i;
+                setTimeout(function() {
+                    $(val).removeClass('invisible').addClass('element-in-view');
+                        // console.log('This is index: ' + i);
+                    if(i == 0 ) {
+                        fireAnimatedEmployees();
+                    } else if(i==1) {
+                        fireAnimate6();
+                    } else if(i==3) {
+                        fireAnimate23();
+                    } else if(i==4) {
+                        fireAnimate70();
+                    }
+                }, time);
+            }, 2000);
+        });
+
+	}
+
     console.log('This is from give in:');
 
     // // DECLARE VARS
@@ -25,9 +165,21 @@ jQuery(document).ready(function($) {
     // PAGES FOR HEADING ANINATIONS
    	var pages = $('#c3xgm-about-page-container > .c3xgm-about-page'),
    		sections = $('#c3xgm-about-page-our-commitment > .c3xgm-about-page'),	
-    	blocks = $('.c3xgm-about-block').not('.c3xgm-about-solar-panels-animation, .c3xgm-about-test-facility'),
+   		endNav = $('#c3xgm-about-end-nav'),
+    	blocks = $('.c3xgm-about-block').not('.c3xgm-about-solar-panels-animation, .c3xgm-about-test-facility, #c3xgm-about-page-our-people .c3xgm-about-block'),
     	sliders = $('.c3xgm-about-module-foundation, .c3xgm-about-module-technology, .c3xgm-about-module-car'),
-    	triggers = $('#animate-flag-line, #c3xgm-about-solar-road-list');
+    	triggers = $('#animate-flag-line, #c3xgm-about-solar-road-list'),
+    	ourPeopleBlocks = $('#c3xgm-about-page-our-people .c3xgm-about-block');
+    	allAnimatedElements = [pages, sections, endNav, blocks, ourPeopleBlocks];
+
+    // HIDE ANIMATED ELEMENTS
+    // $(pages).addClass('invisible');
+
+    $.each(allAnimatedElements, function(i,elem) {
+    	$(this).addClass('invisible');
+    });
+
+
 
     // console.log('These are your pages:');
     // console.dir(pages);
@@ -38,6 +190,7 @@ jQuery(document).ready(function($) {
     // console.log('These are your triggers:');
     // console.dir(triggers);
 
+    // ADD BLOCKS TO pages objectsd
 
     // BIND NAV UPDATE TO PAGES WHEN IN VIEW
     // PAGE AND NAV FUNCTIONALITY
@@ -52,13 +205,18 @@ jQuery(document).ready(function($) {
 			console.log('PAGE in view!: ' + name);
 			console.log('This is currentPage: ' + currentPage);
 			console.dir(this);
+
+			// IF CURRENT PAGE == OUR PEOPLE (1) - THEN FIRE OFF ANIMATIONS
+			if(currentPage == 1) {
+				fireOurPeopleAnimations();
+			}
 			
 			// UNHIGHTLIGHT CURRENT NAV BULLET
-			$(".c3xgm-about-nav-bullet-active").removeClass("c3xgm-about-nav-bullet-active");
+			$(".c3xgm-about-main-nav > li > a.c3xgm-about-nav-bullet-active").removeClass("c3xgm-about-nav-bullet-active");
 			// HIGHLIGHT NAV BULLET
 			$(navLinks.eq(currentPage)).addClass("c3xgm-about-nav-bullet-active");
 			// ADD PAGE IN VIEW CLASS
-			$(this).addClass('c3xgm-about-page-in-view'); 
+			$(this).removeClass('invisible').addClass('c3xgm-about-page-in-view'); 
 
 		} else {
 			console.log('PAGE out of view!: ' + name);
@@ -81,14 +239,16 @@ jQuery(document).ready(function($) {
 			
 			// UNHIGHTLIGHT CURRENT SUB NAV BULLET
 			$(".c3xgm-about-main-nav-sub .c3xgm-about-nav-bullet-active").removeClass("c3xgm-about-nav-bullet-active");
+			
 			// HIGHLIGHT SUB NAV BULLET
-			var subNavHighlight = currentSection + 4;
-			// $(navLinks.eq(subNavHighlight)).addClass("c3xgm-about-nav-bullet-active");
+			var subNavLink = $('a[href="'+id+'"]');
+			console.log('This is subNavLink: ' + subNavLink);
+			console.dir(subNavLink);
 
-			console.log('This is subNavHighlight: '+ subNavHighlight);
-			console.dir($(navLinks.eq(subNavHighlight)));
+			$(subNavLink).eq(0).addClass("c3xgm-about-nav-bullet-active");
+			
 			// ADD SECTION IN VIEW CLASS
-			$(this).addClass('c3xgm-about-page-in-view'); 
+			$(this).removeClass('invisible').addClass('c3xgm-about-page-in-view');
 
 		} else {
 			console.log('SECTION out of view!: ' + name);
@@ -145,6 +305,30 @@ jQuery(document).ready(function($) {
 			} else if (name == 'c3xgm-about-module-foundation') {
 				killAutoRotateFoundationSlider();
 			}
+		}
+	});
+
+
+	// BLOCKS
+    $(blocks).bind('inview', function (event, visible) {
+    	var name = getName(this);
+		if (visible == true) {
+			console.log('BLOCK in view!: ' + name);
+			$(this).removeClass('invisible').addClass('element-in-view');
+		} else {
+			console.log('BLOCK out of view!: ' + name);
+		}
+	});
+
+
+	// END NAV
+    $(endNav).bind('inview', function (event, visible) {
+    	var name = getName(this);
+		if (visible == true) {
+			console.log('endNav in view!: ' + name);
+			$(this).removeClass('invisible').addClass('c3xgm-about-page-in-view');
+		} else {
+			console.log('endNav out of view!: ' + name);
 		}
 	});
 
