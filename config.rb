d8b3b11dccf14afcec73c1a9fa138358e1f7f80a
Jason Tordsen <jason@Jasons-MBP.home>
2015-07-01 23:29:50 -0400

    removing unused files

--- config.rb
+++ config.rb
@@ -3,10 +3,12 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
+output_style = :compressed
+line_comments = false
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
